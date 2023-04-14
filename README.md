ee8e11af2e0baa0f1128c437a8c0fb79b2f0037d
Tomasz <tomgaek@gmail.com>
2023-04-14 20:22:05 +0100

    set-up webpack and css packages

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Webpack-setup
+
+packages used:
+webpack
+webpack-cli
+css-loader
+style-loader
+sass
+sass-loader
+postcss
+postcss-loader
+webpack-dev-server
+
+plug in: 
+mini-css-extract-plugin - switches directory of css in index.html
+postcss-preset-env
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+module.exports = {
+    mode: "development",
+    plugins: [new MiniCssExtractPlugin()],
+    module: {
+        rules: [
+            {
+                test: /\.(s[ac]|c)ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader, 
+                    "css-loader",  
+                    "postcss-loader",
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+
+    devtool: "source-map",
+    devServer: {
+            static: "./dist", 
+            compress: true,
+        port: 9000,
+        },
+}
